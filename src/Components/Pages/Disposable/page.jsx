f5db724914a633109e8d3765e5641df6b3079d3a
Túlio Diego <diegotulio27@gmail.com>
2026-01-24 08:20:49 -0300

    Remove unused state setter from Decorations, Disposable, and Foods components

--- src/Components/Pages/Disposable/page.jsx
+++ src/Components/Pages/Disposable/page.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { ListItems } from "../../UI/ListItems";
 export default function Disposable() {
-    const [items, setItems] = useState([]);
+    const [items] = useState([]);
     return (
         <>
             {<ListItems title={"Descartáveis"} data={items} />}
